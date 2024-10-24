c4ab8f5796e125666a3fe7dde9f8027c32e8c489
ferhat canşi <ferocansi04@gmail.com>
2024-10-24 16:27:43 +0300

    first version

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const email = "ferocansi04@gmail.com";
+const login = " Ferocansi04@gmail.com";
+console.log(login);
+const lowerLogin = login.toLowerCase();
+console.log(lowerLogin);
+const newLogin = lowerLogin.trim();
+console.log(newLogin);
